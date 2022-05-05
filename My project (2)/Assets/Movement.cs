089eaf8e6b14da3c56bbea794efd7b4d84c89a44
Millionaire Whale <78145920+MillionaireWhale@users.noreply.github.com>
2022-05-05 22:15:13 +1200

    Movement script added

---
+++ My project (2)/Assets/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    private Rigidbody2D playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (isOnGround)
+            playerRb.AddForce(Vector2.right * horizontalInput * speed, ForceMode2D.Force);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+}
